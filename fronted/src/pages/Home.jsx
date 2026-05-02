31b3e45c841c9d8be92a778b1312bb6374bcffb4
Amit Kumar <amitkumar9973253016@gmail.com>
2026-05-02 15:15:18 +0530

    fix case-sensitive import errors

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Speciality from '../components/Speciality'
 import TopDoctor from '../components/TopDoctor'
-import Banner from '../components/banner'
+import Banner from '../components/Banner'
 
 export default function Home() {
   return (
